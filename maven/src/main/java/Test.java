ea2914ed61a59b51514f1c31aca943e3a2878910
lxxxxxx808 <616361431@qq.com>
2020-07-22 12:06:22 +0800

    写了个主方法

---
+++ maven/src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @author Lx
+ * @Package PACKAGE_NAME
+ * @date 2020/7/22 12:04
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
